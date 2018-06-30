90a6944c93a6f909c6a7119e10d9c10b8147b717
Aingon <ainelainel@hotmail.com>
2018-06-30 15:06:22 +0000

    update db url

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ var express         = require("express"),
 var commentRoutes   = require("./routes/comments"),
     campgroundRoutes= require("./routes/campgrounds"),
     indexRoutes     = require("./routes/index")
-    
-    
-//mongoose.connect("mongodb://localhost/yelp_camp_v3");
-mongoose.connect("mongodb://colt:password1@ds121861.mlab.com:21861/campreview");
+
+
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://colt:password1@ds121861.mlab.com:21861/campreview");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
